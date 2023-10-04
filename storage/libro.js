773539caeddc7a18d6f82c78d4b72e12eba5721e
“Kevin-Jimenez-D” <kevinjimenezd97@gmail.com>
2023-10-04 10:56:23 -0500

    feat: :sparkles: Una nueva función para obtener un solo dato con el método GET
    En este caso se hizo una modificación a la base de datos de json para poder acceder de forma exitosa al dato

--- storage/libro.js
+++ storage/libro.js
@@ -10,11 +10,26 @@ const uri = `${env.ssl+env.hostName}:${env.port}`;
 
 const config ={method:undefined, headers:{"Content-Type":"application/json"}};
 
-//Método modularizado para obtener datos con GET
+
+
+//Método modularizado para obtener datos con GET INICIO
 export const getAll = async()=>{
     config.method="GET";
     let res = await (await fetch(`${uri}/libro`,config)).json();
     return res;
 }
 //Obtener esos datos en la consola
-console.log(await getAll());
+//console.log(await getAll());
+//Método modularizado para obtener datos con GET FIN
+
+
+
+//Método modularizado para obtener solo un dato con GET INICIO
+//Mirar que en la parte de la base de datos, debe estar id con el numero y con eso se accedera al dato en específico
+export const getOne = async(id)=>{
+    config.method="GET";
+    let res = await (await fetch(`${uri}/libro/${id}`,config) ).json();
+    return res;
+}
+console.log(await getOne(2));
+//Método modularizado para obtener solo un dato con GET FIN
